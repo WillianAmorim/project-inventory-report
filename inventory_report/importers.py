16fdb019290514a484cdb8caee68ebedef1e1383
Willian <willian.amorim1@outlook.com>
2023-07-17 18:58:57 -0300

    Finalizado requisito 1

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,8 +1,15 @@
-from typing import Dict, Type
+from typing import Dict, List, Type
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
 
 
-class Importer:
-    ...
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
 
 
 class JsonImporter:
